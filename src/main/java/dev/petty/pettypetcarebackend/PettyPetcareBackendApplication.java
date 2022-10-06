7fce518d7b74d8f8fa0fe7ed7e9f47836d29d332
KumaMUMI <olozatarebunk@gmail.com>
2022-10-06 22:55:21 +0700

    first commit

---
+++ src/main/java/dev/petty/pettypetcarebackend/PettyPetcareBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.petty.pettypetcarebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PettyPetcareBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PettyPetcareBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/petty/pettypetcarebackend/PettyPetcareBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.petty.pettypetcarebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PettyPetcareBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
